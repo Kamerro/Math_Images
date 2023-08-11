00ba00f59f32a4882aab7a5b6dcfc6116b3938a0
Kamil Mikuła <kamimik090@student.polsl.pl>
2023-08-11 16:02:37 +0200

    k

--- Math_Images/Form1.Designer.cs
+++ Math_Images/Form1.Designer.cs
@@ -29,6 +29,9 @@
         private void InitializeComponent()
         {
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btn_smooth = new System.Windows.Forms.Button();
+            this.btn_cross = new System.Windows.Forms.Button();
+            this.btn_circle = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -39,13 +42,46 @@
             this.pictureBox1.Size = new System.Drawing.Size(310, 266);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
-            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseHover += new System.EventHandler(this.pictureBox1_MouseHover);
+            // 
+            // btn_smooth
+            // 
+            this.btn_smooth.Location = new System.Drawing.Point(329, 13);
+            this.btn_smooth.Name = "btn_smooth";
+            this.btn_smooth.Size = new System.Drawing.Size(191, 37);
+            this.btn_smooth.TabIndex = 1;
+            this.btn_smooth.Text = "smooth circle";
+            this.btn_smooth.UseVisualStyleBackColor = true;
+            this.btn_smooth.Click += new System.EventHandler(this.btn_smooth_Click);
+            // 
+            // btn_cross
+            // 
+            this.btn_cross.Location = new System.Drawing.Point(329, 56);
+            this.btn_cross.Name = "btn_cross";
+            this.btn_cross.Size = new System.Drawing.Size(191, 37);
+            this.btn_cross.TabIndex = 2;
+            this.btn_cross.Text = "cross";
+            this.btn_cross.UseVisualStyleBackColor = true;
+            this.btn_cross.Click += new System.EventHandler(this.btn_cross_Click);
+            // 
+            // btn_circle
+            // 
+            this.btn_circle.Location = new System.Drawing.Point(328, 99);
+            this.btn_circle.Name = "btn_circle";
+            this.btn_circle.Size = new System.Drawing.Size(191, 37);
+            this.btn_circle.TabIndex = 3;
+            this.btn_circle.Text = "circle";
+            this.btn_circle.UseVisualStyleBackColor = true;
+            this.btn_circle.Click += new System.EventHandler(this.btn_circle_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(334, 291);
+            this.ClientSize = new System.Drawing.Size(532, 291);
+            this.Controls.Add(this.btn_circle);
+            this.Controls.Add(this.btn_cross);
+            this.Controls.Add(this.btn_smooth);
             this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -57,6 +93,9 @@
         #endregion
 
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btn_smooth;
+        private System.Windows.Forms.Button btn_cross;
+        private System.Windows.Forms.Button btn_circle;
     }
 }
 
--- Math_Images/Form1.cs
+++ Math_Images/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Diagnostics;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,46 +20,8 @@ namespace Math_Images
             pictureBox1.Image = (Image)new Bitmap(pictureBox1.Width, pictureBox1.Height);
         }
 
-        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
-        {
-            Stopwatch sw = Stopwatch.StartNew();
-            Point p = e.Location;
-            PictureBox thisSender = (sender as PictureBox);
-            WyzerujKolor(thisSender);
-            Bitmap thisBitmap = thisSender.Image as Bitmap;
-            #region params
-            int setOff = 100;
-            int arm = setOff / 2;
-            int armSquared = arm * arm;
-            int left = p.X - arm;
-            int right = p.X + arm;
-            int top = p.Y - arm;
-            int bottom = p.Y + arm;
-            #endregion
-            for (int i = left; i <= right; i++)
-            {
-                for(int j = top; j <= bottom; j++)
-                {
-                    int dx = i - e.Location.X;
-                    int dy = j - e.Location.Y;
-                    double distanceSquared = dx*dx + dy*dy;
-                    if ( distanceSquared <= armSquared)
-                    {
-                        if (i > 0 && i < thisBitmap.Width && j > 0 && j < thisBitmap.Height)
-                        {
-                                double normalizowany_dist = distanceSquared/ (double)armSquared;
-                                int wartosc = (int)(normalizowany_dist * 255);
-                                Color col = Color.FromArgb(wartosc, wartosc, wartosc);
-                                thisBitmap.SetPixel(i, j, col);
-                        }
-                        }
-                    }
-            }
-            thisSender.Image = thisBitmap;
-            sw.Stop();
-            MessageBox.Show(sw.ElapsedMilliseconds.ToString());
-        }
 
+        
         private void WyzerujKolor(PictureBox sender)
         {
             var thisBitmap = sender.Image as Bitmap;
@@ -71,5 +34,150 @@ namespace Math_Images
             }
             sender.Image = thisBitmap;
         }
+
+        private void pictureBox1_MouseHover(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_smooth_Click(object sender, EventArgs e)
+        {
+            pictureBox1.MouseMove -= methodCross;
+            pictureBox1.MouseMove += methodSmooth;
+        }
+        private void btn_cross_Click(object sender, EventArgs e)
+        {
+            pictureBox1.MouseMove -= methodSmooth;
+            pictureBox1.MouseMove += methodCross;
+        }
+        private void btn_circle_Click(object sender, EventArgs e)
+        {
+            pictureBox1.MouseMove -= methodSmooth;
+            pictureBox1.MouseMove -= methodCross;
+            pictureBox1.MouseMove += methodCircle;
+        }
+        private void methodCross(object sender, MouseEventArgs e)
+        {
+            Point p = e.Location;
+            PictureBox thisSender = (sender as PictureBox);
+            WyzerujKolor(thisSender);
+            Bitmap thisBitmap = thisSender.Image as Bitmap;
+            #region params
+            int setOff = 100;
+            int arm = setOff / 2;
+            int armSquared = arm * arm;
+            int left = p.X - arm;
+            int right = p.X + arm;
+            int top = p.Y - arm;
+            int bottom = p.Y + arm;
+            int locationX = e.Location.X;
+            int locationY = e.Location.Y;
+            #endregion
+            for (int i = left; i <= right; i++)
+            {
+                for (int j = top; j <= bottom; j++)
+                {
+                    if (i > 0 && i < thisBitmap.Width && j > 0 && j < thisBitmap.Height)
+                    {
+                        int dx = i - locationX;
+                        int dy = j - locationY;
+                        double distanceSquared = Math.Pow(Math.Cos(dx),2) + Math.Pow(Math.Sin(dy),2);
+                        if (Math.Sqrt(distanceSquared) == 1)
+                        {
+                            if (i > 0 && i < thisBitmap.Width && j > 0 && j < thisBitmap.Height)
+                            {
+    
+                                thisBitmap.SetPixel(i, j, Color.Black);
+                            }
+                        }
+                    }
+                }
+            }
+            thisSender.Image = thisBitmap;
+        }
+
+        private void methodSmooth(object sender, MouseEventArgs e)
+        {
+            Point p = e.Location;
+            PictureBox thisSender = (sender as PictureBox);
+            WyzerujKolor(thisSender);
+            Bitmap thisBitmap = thisSender.Image as Bitmap;
+            #region params
+            int setOff = 100;
+            int arm = setOff / 2;
+            int armSquared = arm * arm;
+            int left = p.X - arm;
+            int right = p.X + arm;
+            int top = p.Y - arm;
+            int bottom = p.Y + arm;
+            int locationX = e.Location.X;
+            int locationY = e.Location.Y;
+            #endregion
+            for (int i = left; i <= right; i++)
+            {
+                for (int j = top; j <= bottom; j++)
+                {
+                    if (i > 0 && i < thisBitmap.Width && j > 0 && j < thisBitmap.Height)
+                    {
+                        int dx = i - locationX;
+                        int dy = j - locationY;
+                        double distanceSquared = dx * dx + dy * dy;
+                        if (distanceSquared <= armSquared)
+                        {
+                            if (i > 0 && i < thisBitmap.Width && j > 0 && j < thisBitmap.Height)
+                            {
+                                double normalizowany_dist = distanceSquared / (double)armSquared;
+                                int wartosc = (int)(normalizowany_dist * 255);
+                                Color col = Color.FromArgb(wartosc, wartosc, wartosc);
+                                thisBitmap.SetPixel(i, j, col);
+                            }
+                        }
+                    }
+                }
+            }
+            thisSender.Image = thisBitmap;
+        }
+
+        private void methodCircle(object sender, MouseEventArgs e)
+        {
+            Point p = e.Location;
+            PictureBox thisSender = (sender as PictureBox);
+            WyzerujKolor(thisSender);
+            Bitmap thisBitmap = thisSender.Image as Bitmap;
+            #region params
+            int setOff = 100;
+            int arm = setOff / 2;
+            int armSquared = arm * arm;
+            int left = p.X - arm;
+            int right = p.X + arm;
+            int top = p.Y - arm;
+            int bottom = p.Y + arm;
+            int locationX = e.Location.X;
+            int locationY = e.Location.Y;
+            #endregion
+            for (int i = left; i <= right; i++)
+            {
+                for (int j = top; j <= bottom; j++)
+                {
+                    if (i > 0 && i < thisBitmap.Width && j > 0 && j < thisBitmap.Height)
+                    {
+                        int dx = i - locationX;
+                        int dy = j - locationY;
+                        double distance = Math.Sqrt(dx * dx + dy * dy);
+                        if (distance-arm > -0.5 && distance-arm < 0.5)
+                        {
+                            if (i > 0 && i < thisBitmap.Width && j > 0 && j < thisBitmap.Height)
+                            {
+
+                                thisBitmap.SetPixel(i, j, Color.Black);
+                            }
+                        }
+                    }
+                }
+            }
+            thisSender.Image = thisBitmap;
+        }
+
+      
     }
 }
